c76741638317cc27da5db25e6135acf3042bd67c
avglinsky <avglinsky@yandex.ru>
2020-06-20 16:48:13 +0300

    second task 4 commit

--- init.sh
+++ init.sh
@@ -1,12 +1,6 @@
-#nginx conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-
-#gunicorn conf
-sudo rm /etc/gunicorn.d/test
-sudo rm /etc/gunicorn.d/ask
-#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn-ask.conf /etc/gunicorn.d/ask
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
