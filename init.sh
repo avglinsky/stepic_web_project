f8148c5138b1aef7615861bf1c140e94689e1108
avglinsky <avglinsky@yandex.ru>
2020-06-26 18:09:14 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
+sudo apt-get update
+sudo apt-get install -y python3.5
+sudo apt-get install -y python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo pip3 install --upgrade pip
+sudo pip3 install --upgrade django==2.0.7
+sudo pip3 install --upgrade gunicorn
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
